1250bfc808ae8093eb86ae72fdf1b7f03de26674
Oluwaseyi F <75592533+xOluwaseyi@users.noreply.github.com>
2022-01-12 10:58:42 +0100

    Edited header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>SeyiMeals</h1>
+        <h3>SeyiFoods</h3>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
